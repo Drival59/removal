587d34c22a4eb80bee9dd49285a05e4714c81af7
Drival59 <valentin.hanard@laposte.net>
2018-07-24 21:39:05 +0200

    finalisation edition commentaires

--- src/RemovalBundle/Controller/CommentController.php
+++ src/RemovalBundle/Controller/CommentController.php
@@ -2,6 +2,8 @@
 
 namespace RemovalBundle\Controller;
 
+use RemovalBundle\Form\CommentType;
+use Symfony\Component\HttpFoundation\Request;
 
 class CommentController extends MasterController
 {
@@ -18,13 +20,39 @@ class CommentController extends MasterController
     ));
   }
 
+  public function editAction($newsUrl, $commentId, Request $request)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $user = $this->getUser();
+    $comment = $em->getRepository('RemovalBundle:Comment')->findOneById($commentId);
+    $commentByUser = $em->getRepository('RemovalBundle:Comment')->myFindOneByCommentUser($commentId, $user);
+    if ($comment != null AND (count($commentByUser) != 0 || $this->isGranted('ROLE_SB'))) {
+      $form = $this->createForm(CommentType::class, $comment);
+      $form->handleRequest($request);
+      if ($form->isSubmitted()) {
+        $em->merge($comment);
+        $em->flush();
+        $this->addFlash('noticeComment', 'Le commentaire a bien été modifié.');
+        return $this->redirectToRoute('removal_news_read', array(
+          'newsUrl' => $newsUrl,
+        ));
+      }
+      return $this->render('@Removal/Comment/edit.html.twig', array(
+        'form' => $form->createView(),
+      ));
+    }
+    return $this->redirectToRoute('removal_news_read', array(
+      'newsUrl' => $newsUrl,
+    ));
+  }
+
   public function deleteAction($newsUrl, $commentId)
   {
     $em = $this->getDoctrine()->getManager();
     $user = $this->getUser();
     $comment = $em->getRepository('RemovalBundle:Comment')->findOneById($commentId);
     $commentByUser = $em->getRepository('RemovalBundle:Comment')->myFindOneByCommentUser($commentId, $user);
-    if ($comment != null AND count($commentByUser) != 0) {
+    if ($comment != null AND (count($commentByUser) != 0 || $this->isGranted('ROLE_SB'))) {
       $em->remove($comment);
       $em->flush();
       $this->addFlash('noticeComment', 'Le commentaire a bien été supprimé.');
